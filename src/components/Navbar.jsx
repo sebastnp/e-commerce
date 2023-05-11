45929c5d0c55ff9058a9b4f70431af47b4935701
sebastnp <sebastian_27_12_98@hotmail.com>
2023-05-11 11:29:10 -0500

    testing cart measures

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,8 +6,6 @@ import { useScrollPosition } from '../hooks/useScrollPosition'
 import { Link } from 'react-router-dom'
 import styles from '../styles/Navbar.css'
 import Logo from '../../media/imagenes/logo.png'
-import { BsBagDash } from 'react-icons/bs'
-import { FaBars, FaTimes } from 'react-icons/fa'
 
 export function Navbar() {
   const { isMenuOpen, toggleMenu } = useCart()
@@ -44,8 +42,8 @@ export function Navbar() {
           <button className='nav-btn nav-close-btn' onClick={showNavbar}>
             <svg
               xmlns='http://www.w3.org/2000/svg'
-              width='40'
-              height='40'
+              width='30'
+              height='30'
               viewBox='0 0 24 24'
             >
               <path
