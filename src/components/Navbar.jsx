0570a3aa01f54ecffd98beddcb644c9c91493156
sebastnp <sebastian_27_12_98@hotmail.com>
2023-05-11 11:06:03 -0500

    add svg icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -46,14 +46,35 @@ export function Navbar() {
           </button>
         </div>
         <button className='cart-btn' onClick={toggleMenu}>
-          <BsBagDash className='cart-btn_icon' />
+          {/* <BsBagDash className='cart-btn_icon' /> */}
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='35'
+            height='35'
+            viewBox='0 0 24 24'
+          >
+            <path
+              fill='currentColor'
+              d='M4 22V6h4q0-1.65 1.175-2.825T12 2q1.65 0 2.825 1.175T16 6h4v16H4Zm6-16h4q0-.825-.588-1.413T12 4q-.825 0-1.413.588T10 6ZM6 20h12V8h-2v3h-2V8h-4v3H8V8H6v12Zm0 0V8v12Z'
+            />
+          </svg>
         </button>
         <Offcanvas isOpen={isMenuOpen} onClose={toggleMenu}>
           <Cart onClose={toggleMenu} />
         </Offcanvas>
         {/* Icono de opciones que se mostrara cuando la pantalla sea pequeña */}
         <button className='nav-btn' onClick={showNavbar}>
-          <FaBars className='nav-btn_icon' />
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='30'
+            height='30'
+            viewBox='0 0 448 512'
+          >
+            <path
+              fill='currentColor'
+              d='M0 96c0-17.7 14.3-32 32-32h384c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zm0 160c0-17.7 14.3-32 32-32h384c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zm448 160c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32h384c17.7 0 32 14.3 32 32z'
+            />
+          </svg>
         </button>
       </nav>
     </div>
